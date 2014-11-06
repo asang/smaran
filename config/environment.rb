5809830129f4942ce583d6e7a7e4ed7f23d720ce
Asang Dani (asangmac) <asang.dani@gmail.com>
2014-11-06 10:06:42 +0530

    Allow .cfg file types to be uplodated as application/octet-stream mime type

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Paperclip.options[:content_type_mappings] = {
+  :cfg => "application/octet-stream"
+}
+
 # Initialize the Rails application.
 Smaran::Application.initialize!
